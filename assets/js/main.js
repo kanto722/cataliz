556b9f217711726ad809a61c36aad213a5ecfda6
Kanto Kanto72 <kanto72@yandex.ru>
2021-03-28 13:03:17 +0300

    children sleep

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+    },
+  
+  });
