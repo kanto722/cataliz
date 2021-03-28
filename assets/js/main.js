7e06c335f4c1a8b14298e9687e03a98eaac0f228
Kanto Kanto72 <kanto72@yandex.ru>
2021-03-28 17:37:20 +0300

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,21 @@ const swiper = new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
     },
-  
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 120
+      }
+    }
   });
